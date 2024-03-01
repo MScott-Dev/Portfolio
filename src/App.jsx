1ceefb3d171258d6fe4ec35e4700a4473359b4c9
MScott-Dev <MScott0199@gmail.com>
2024-03-01 14:55:18 -0500

    Added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import NavigationBar from './components/NavigationBar'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <NavigationBar />
   )
 }
 
