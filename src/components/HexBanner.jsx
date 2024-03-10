f328742094c09de5454087b0b924f35356f181dc
MScott-Dev <MScott0199@gmail.com>
2024-03-09 19:09:55 -0500

    Responsive text size in banner

--- src/components/HexBanner.jsx
+++ src/components/HexBanner.jsx
@@ -2,7 +2,7 @@ export default function HexBanner(props) {
     return (
         <section className="w-full h-[20rem] banner border-t border-b border-teal">
             <section className='flex justify-center flex-wrap content-center h-full'>
-                <h1 className="text-8xl text-Offwhite font-semibold border-b mb-3 background">{props.title}</h1>
+                <h1 className="lg:text-8xl md:text-7xl sm:text-6xl text-6xl text-Offwhite font-semibold border-b mb-3 background">{props.title}</h1>
             </section>
         </section>
     )
