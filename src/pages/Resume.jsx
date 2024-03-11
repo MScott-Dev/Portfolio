d858aa0628ca0320b2490b6a9c9d7748517a1d86
MScott-Dev <MScott0199@gmail.com>
2024-03-11 16:07:49 -0400

    Download button working

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ export default function ResumePage() {
     return (
         <section>
             <section className="flex flex-wrap justify-center">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-Offwhite ">Download my resume!</h2>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-Offwhite mt-3">Download my resume!</h2>
             </section>
             <section className="flex flex-wrap justify-center">
                 <img className=" sm:w-3/5 md:w-3/5 lg:w-[40rem] h-auto p-5" src={Resume} />
