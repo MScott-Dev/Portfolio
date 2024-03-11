77787887e609b617a9d951bfd5fd9757cc07b706
MScott-Dev <MScott0199@gmail.com>
2024-03-11 16:24:30 -0400

    Netlify form setup

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,7 +4,7 @@ export default function ContactForm() {
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-Offwhite ">Get in touch with me!</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-Offwhite sm:text-xl">I&apos;d Love to Hear from You</p>
-                <form action="#" className="space-y-8">
+                <form action="#" className="space-y-8" netlify>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-Offwhite ">Your email</label>
                         <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@gmail.com" required></input>
